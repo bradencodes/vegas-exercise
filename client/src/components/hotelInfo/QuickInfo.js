68d64819175d6b6fec42970f641c303605f121b0
Braden Walker <bradencodes@gmail.com>
2019-10-22 15:13:54 -0700

    style info in quick info

--- client/src/components/hotelInfo/QuickInfo.js
+++ client/src/components/hotelInfo/QuickInfo.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 const QuickInfo = ({ hotelObj }) => {
-  const generateStars = (num) => {
+  const generateStars = num => {
     let stars = '';
-    for (let i = 0 ; i < num ; i++) {
-      stars += '★'
+    for (let i = 0; i < num; i++) {
+      stars += '★';
     }
     return stars;
-  }
+  };
 
   return (
     <div className='quickInfo'>
-      <span className="name" data-rating={generateStars(hotelObj.starRating)}>{hotelObj.name.toUpperCase()}</span>
+      <h1 className='name' data-rating={generateStars(hotelObj.starRating)}>
+        {hotelObj.name.toUpperCase()}
+      </h1>
+      <aside className='info'>
+        <button className='item' data-icon='&#x2352;'>
+          {hotelObj.location.areaName}
+        </button>
+        <p className='item' data-icon='&#x2706;'>
+          {hotelObj.phoneNumber}
+        </p>
+        <p className='item' data-icon='&#x2764;'>
+          Best Price Guarantee
+        </p>
+      </aside>
     </div>
-  )
-}
+  );
+};
 
 export default QuickInfo;
